b94e9ff71c9500c816cbae104c036247749caca8
Frederic Malo <frederic.malo@checkmarx.com>
2026-08-28 15:21:31 +0200

    Checkmarx Bot - Auto Pull Request

--- test/e2e/integration/contributions_spec.js
+++ test/e2e/integration/contributions_spec.js
@@ -52,4 +52,142 @@ describe("/contributions behaviour", () => {
 
     cy.url().should("include", "contributions");
   });
+
+  // Security tests: CWE-94 Code Injection (SSJS Injection) prevention
+  // These tests verify that eval() has been replaced with parseInt() so that
+  // user-supplied JavaScript expressions cannot be executed on the server.
+
+  it("Should reject JavaScript expression injection in preTax field", () => {
+    // An attacker might send a JS expression; parseInt() returns NaN, triggering validation error
+    cy.userSignIn();
+    cy.visitPage("/contributions");
+
+    cy.get("table")
+      .find("input")
+      .eq(0)
+      .clear()
+      .type("1+1");
+
+    cy.get("button[type='submit']")
+      .click();
+
+    // The server must reject this input with a validation error, not execute it
+    cy.get(".alert-danger, .alert-warning, [class*='error'], [class*='alert']")
+      .should("be.visible");
+  });
+
+  it("Should reject JavaScript expression injection in afterTax field", () => {
+    cy.userSignIn();
+    cy.visitPage("/contributions");
+
+    cy.get("table")
+      .find("input")
+      .eq(1)
+      .clear()
+      .type("2+3");
+
+    cy.get("button[type='submit']")
+      .click();
+
+    cy.get(".alert-danger, .alert-warning, [class*='error'], [class*='alert']")
+      .should("be.visible");
+  });
+
+  it("Should reject JavaScript expression injection in roth field", () => {
+    cy.userSignIn();
+    cy.visitPage("/contributions");
+
+    cy.get("table")
+      .find("input")
+      .eq(2)
+      .clear()
+      .type("5*2");
+
+    cy.get("button[type='submit']")
+      .click();
+
+    cy.get(".alert-danger, .alert-warning, [class*='error'], [class*='alert']")
+      .should("be.visible");
+  });
+
+  it("Should reject non-numeric string inputs", () => {
+    cy.userSignIn();
+    cy.visitPage("/contributions");
+
+    cy.get("table")
+      .find("input")
+      .eq(0)
+      .clear()
+      .type("abc");
+
+    cy.get("button[type='submit']")
+      .click();
+
+    cy.get(".alert-danger, .alert-warning, [class*='error'], [class*='alert']")
+      .should("be.visible");
+  });
+
+  it("Should reject negative contribution values", () => {
+    cy.userSignIn();
+    cy.visitPage("/contributions");
+
+    cy.get("table")
+      .find("input")
+      .eq(0)
+      .clear()
+      .type("-5");
+
+    cy.get("button[type='submit']")
+      .click();
+
+    cy.get(".alert-danger, .alert-warning, [class*='error'], [class*='alert']")
+      .should("be.visible");
+  });
+
+  it("Should reject contributions exceeding 30% total", () => {
+    cy.userSignIn();
+    cy.visitPage("/contributions");
+
+    const inputs = cy.get("table").find("input");
+    inputs.eq(0).clear().type("20");
+    inputs.eq(1).clear().type("10");
+    inputs.eq(2).clear().type("5");
+
+    cy.get("button[type='submit']")
+      .click();
+
+    cy.get(".alert-danger, .alert-warning, [class*='error'], [class*='alert']")
+      .should("be.visible");
+  });
+
+  it("Should accept valid numeric contributions within 30% limit", () => {
+    cy.userSignIn();
+    cy.visitPage("/contributions");
+
+    cy.get("table")
+      .find("input")
+      .eq(0)
+      .clear()
+      .type("5");
+
+    cy.get("table")
+      .find("input")
+      .eq(1)
+      .clear()
+      .type("5");
+
+    cy.get("table")
+      .find("input")
+      .eq(2)
+      .clear()
+      .type("5");
+
+    cy.get("button[type='submit']")
+      .click();
+
+    cy.get(".alert-success")
+      .should("be.visible");
+
+    cy.url().should("include", "contributions");
+  });
 });
